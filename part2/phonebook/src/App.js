cdfb405eba6a80b6d2c6ff203618a75b8927ee98
TameemShahid <tameem_shahid@yahoo.com>
2021-03-21 02:01:09 +0500

    Completed Exercise 2.16, create separate services module for communicating with backend

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,14 @@ import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Person from "./components/Person";
-import axios from "axios";
+import personsService from "./services/persons";
 
 const App = () => {
-  // const [persons, setPersons] = useState([
-  //   { name: "Arto Hellas", number: "040-123456" },
-  //   { name: "Ada Lovelace", number: "39-44-5323523" },
-  //   { name: "Dan Abramov", number: "12-43-234345" },
-  //   { name: "Mary Poppendieck", number: "39-23-6423122" },
-  // ]);
-
   const [persons, setPersons] = useState([]);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled!");
-      setPersons(response.data);
+    personsService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -60,14 +51,12 @@ const App = () => {
       setNewNumber("");
     } else {
       const newPerson = { name: newName, number: newNumber };
-      axios
-        .post("http://localhost:3001/persons", newPerson)
-        .then((response) => {
-          setPersons(persons.concat(response.data));
-          setNewName("");
-          setNewNumber("");
-          setSearchResult(persons);
-        });
+      personsService.create(newPerson).then((newCreatedPerson) => {
+        setPersons(persons.concat(newCreatedPerson));
+        setNewName("");
+        setNewNumber("");
+        setSearchResult(persons);
+      });
     }
   };
 

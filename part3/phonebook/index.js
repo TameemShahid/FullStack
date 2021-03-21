1078cc42d87b1404c3b6173962c90f7a9cda0537
TameemShahid <tameem_shahid@yahoo.com>
2021-03-22 03:58:57 +0500

    Completed Exercise 3.4, Implemented the DELETE route for a specific person

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -61,6 +61,14 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+// DELETE Route for a person
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;

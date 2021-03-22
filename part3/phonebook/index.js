789b142a31c9fa5010cca4b2cbfcdf24c5cdf069
TameemShahid <tameem_shahid@yahoo.com>
2021-03-23 02:08:08 +0500

    Completed Exercise 3.9, backend is now working with frontend, fixed the path in frontend to make calls to /api/persons and imported cors module in backend to enable CORS

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const { json } = require("express");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
 
 let persons = [
   {

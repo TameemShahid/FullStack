3333bfe18b68cd81a741f08fac5233f71c9e57e6
TameemShahid <tameem_shahid@yahoo.com>
2021-03-23 03:39:24 +0500

    Completed till Exercise 3.11, phonebook application frontend is in part2/phonebookFrontend where as the application backend is at part3/phonebookBackend

---
+++ part2/phonebookFrontend/src/services/persons.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (personObject) => {
+  const request = axios.post(baseUrl, personObject);
+  return request.then((response) => response.data);
+};
+
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const update = (personObject) => {
+  const request = axios.put(`${baseUrl}/${personObject.id}`, personObject);
+  return request.then((response) => response.data);
+};
+
+export default {
+  getAll,
+  create,
+  remove,
+  update,
+};

f2382dab4112c9f0279180b9bc8ea0f771900026
TameemShahid <tameem_shahid@yahoo.com>
2021-04-09 00:37:28 +0500

    Modified gitignore file, Added Phonebook-Database as a separate folder but mongoose model is present in the Phonebook-Backend/models as well.

--- part3/Phonebook-Database/mongo.js
+++ part3/Phonebook-Database/mongo.js
@@ -1,14 +1,6 @@
 var mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log(
-    "Please provide password to the database: node mongo.js <password>"
-  );
-  process.exit(1);
-}
-
-password = process.argv[2];
-const url = `mongodb+srv://Tameem:${password}@cluster0.54zwv.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, {
   useNewUrlParser: true,
@@ -22,25 +14,4 @@ const personSchema = mongoose.Schema({
   number: String,
 });
 
-const Person = mongoose.model("Person", personSchema);
-
-if (process.argv.length === 3) {
-  Person.find({}).then((result) => {
-    result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
-} else if (process.argv.length === 5) {
-  const personName = process.argv[3];
-  const personNumber = process.argv[4];
-  const person = new Person({
-    name: personName,
-    number: personNumber,
-  });
-
-  person.save().then((response) => {
-    console.log(`Added ${personName} with number ${personNumber} to phonebook`);
-    mongoose.connection.close();
-  });
-}
+module.exports = mongoose.model("Person", personSchema);

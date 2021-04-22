198852c0dc9295fe718f35cfbaf53ff08c4a71da
TameemShahid <tameem_shahid@yahoo.com>
2021-04-23 02:19:12 +0500

    Separated all the blog routes into a separate controller folder. Now blogRouter handles all the route requests for /api/blogs

--- part4/BlogListApp/index.js
+++ part4/BlogListApp/index.js
@@ -1,28 +1,14 @@
-require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Blog = require("./models/mongo");
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blog");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });

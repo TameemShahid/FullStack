73c4491310bed79ef0d76d886fb126c74dd5cdad
TameemShahid <tameem_shahid@yahoo.com>
2021-04-23 02:42:56 +0500

    Separated all the app stuff in separate app.js component. Also import config.js in model component to make connection to db.

--- part4/BlogListApp/index.js
+++ part4/BlogListApp/index.js
@@ -1,14 +1,9 @@
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
 const config = require("./utils/config");
-const blogRouter = require("./controllers/blog");
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogRouter);
+const server = http.createServer(app);
 
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });

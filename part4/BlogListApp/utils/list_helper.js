d03fd987bf277e662bd09634d83f16e374d11b09
TameemShahid <tameem_shahid@yahoo.com>
2021-04-25 01:22:24 +0500

    Completed Exercise 4.5

--- part4/BlogListApp/utils/list_helper.js
+++ part4/BlogListApp/utils/list_helper.js
@@ -10,7 +10,22 @@ const totalLikes = (blogArray) => {
   return blogArray.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogsArray) => {
+  if (blogsArray.length === 0) {
+    return {};
+  } else {
+    let mostLiked = blogsArray[0];
+
+    blogsArray.forEach((blog) => {
+      mostLiked = mostLiked.likes < blog.likes ? blog : mostLiked;
+    });
+
+    return mostLiked;
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

f6c6f6693b8de60dbbe90e37339508d862616a7a
TameemShahid <tameem_shahid@yahoo.com>
2021-04-24 18:02:20 +0500

    Completed Exercise 4.4

--- part4/BlogListApp/utils/list_helper.js
+++ part4/BlogListApp/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogsArray) => {
   return 1;
 };
 
+const totalLikes = (blogArray) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return blogArray.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
